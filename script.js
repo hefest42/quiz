bb9fd7165064649c6e812872be035198ac9eebe8
hefest42 <hefest93@yahoo.com>
2021-04-02 17:45:47 +0200

    added next quesstion button

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const question1 = {
+    option1: ,
+    option2: ,
+    option3: ,
+    option4: ,
+    correct: ,
+}
+const question2 = {
+    option1: ,
+    option2: ,
+    option3: ,
+    option4: ,
+    correct: ,
+}
+const question3 = {
+    option1: ,
+    option2: ,
+    option3: ,
+    option4: ,
+    correct: ,
+}
+const question4 = {
+    option1: ,
+    option2: ,
+    option3: ,
+    option4: ,
+    correct: ,
+}
+const question5 = {
+    option1: ,
+    option2: ,
+    option3: ,
+    option4: ,
+    correct: ,
+}
+
+
+
+// selecting variables
