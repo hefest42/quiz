8627af57390f7744b73a93a268dc3d03cfaa28f3
hefest42 <hefest93@yahoo.com>
2021-04-02 18:20:40 +0200

    selected variables and added questions

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
 "use strict";
 
 const question1 = {
-    option1: ,
-    option2: ,
-    option3: ,
-    option4: ,
-    correct: ,
+    question: "What eminent scientist hosted the television show Cosmos?",
+    option1: "What eminent scientist hosted the television show Cosmos?",
+    option2: "Stephen Hawking",
+    option3: "Carl Sagan",
+    option4: "Albert Einstein",
+    correct: 2,
 }
 const question2 = {
-    option1: ,
-    option2: ,
-    option3: ,
-    option4: ,
-    correct: ,
+    question: "Which of these films parodied the Cold War?",
+    option1: "Patton",
+    option2: "Transformers",
+    option3: "Endless Summer",
+    option4: "Dr.Strangelove",
+    correct: 4,
 }
 const question3 = {
-    option1: ,
-    option2: ,
-    option3: ,
-    option4: ,
-    correct: ,
+    question: "Which song has not won a Eurovision competition?",
+    option1: "Pump Pump",
+    option2: "La, La, La",
+    option3: "Ne partez pas sans moi",
+    option4: "Ding-a-Dong",
+    correct: 1,
 }
 const question4 = {
-    option1: ,
-    option2: ,
-    option3: ,
-    option4: ,
-    correct: ,
+    question: "In which of these countries did manga originate?",
+    option1: "Canada",
+    option2: "Korea",
+    option3: "Japan",
+    option4: "China",
+    correct: 3,
 }
 const question5 = {
-    option1: ,
-    option2: ,
-    option3: ,
-    option4: ,
-    correct: ,
+    question: "Who is the voice of Remy the Rat in Ratatouille?",
+    option1: "Brad Garrett",
+    option2: "Will Patton",
+    option3: "Patton Oswalt",
+    option4: "Will Geer",
+    correct: 3,
 }
 
-
+const questions = [question1, question2, question3, question4, question5];
 
 // selecting variables
+
+// Buttons
+const btnStart = document.querySelector(".btn-start");
+const btnNext = document.querySelector(".btn-next");
+// Labels
+const labelQuestion = document.querySelector(".question");
+const labelOption1 = document.querySelector(".option1");
+const labelOption2 = document.querySelector(".option2");
+const labelOption3 = document.querySelector(".option3");
+const labelOption4 = document.querySelector(".option4");
+// all 4 options
+const options = document.querySelectorAll(".options");
+
+
+
+
+
+
+
+
+
+
+const updateQuestion = function(q) {
+
+}
